72ec224bed9583a30340d13a83e700c3174ca81a
faye <faye.bullwinkel@web.de>
2024-06-20 13:34:10 +0200

    commit unversioned files

---
+++ backend/src/api/climate-adaptation/services/climate-adaptation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * climate-adaptation service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::climate-adaptation.climate-adaptation');
